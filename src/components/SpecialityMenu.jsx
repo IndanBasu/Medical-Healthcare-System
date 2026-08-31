27306367ba98e0d6429760528074aaa6d95f4ece
IndanBasu <basuindan@gmail.com>
2026-08-31 20:11:12 +0530

    Doctors Appointment Page

--- src/components/SpecialityMenu.jsx
+++ src/components/SpecialityMenu.jsx
@@ -22,7 +22,7 @@ const SpecialityMenu = () => {
             key={index}
             to={`/doctors/${item.speciality}`}
             onClick={() => window.scrollTo(0, 0)}
-            className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500"
+            className="flex flex-col items-center text-xs cursor-pointer shrink-0 hover:-translate-y-2.5 transition-all duration-500"
           >
             <img className="w-16 sm:w-24 mb-2" src={item.image} alt="" />
             <p>{item.speciality}</p>
