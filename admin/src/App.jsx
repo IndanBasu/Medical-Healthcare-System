74f26f85f22f758defd0d2d882e284d76867eba7
IndanBasu <basuindan@gmail.com>
2026-09-06 04:10:35 +0530

    Admin Panel for AdminContext, Login, App is setup and issue in Login

---
+++ admin/src/App.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react"
+import Login from "./pages/login.jsx"
+import { ToastContainer, toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+import { AdminContext } from "./context/AdminContext.jsx"
+
+const App = () => {
+  const { aToken } = useContext(AdminContext)
+
+  return aToken ? (
+    <div>
+      <ToastContainer />
+    </div>
+  ) : (
+    <>
+      <Login />
+      <ToastContainer />
+    </>
+  );
+};
+
+export default App
